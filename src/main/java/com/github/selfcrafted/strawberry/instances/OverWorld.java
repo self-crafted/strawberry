583ed03416b2438e119fe1af571c524cc82b8741
offbyone <offbyone@disroot.org>
2022-06-04 14:16:49 +0200

    Generate crafting tables for testing

--- src/main/java/com/github/selfcrafted/strawberry/instances/OverWorld.java
+++ src/main/java/com/github/selfcrafted/strawberry/instances/OverWorld.java
@@ -1,11 +1,16 @@
 package com.github.selfcrafted.strawberry.instances;
 
 import com.github.selfcrafted.strawberry.biomes.Biomes;
+import net.kyori.adventure.text.Component;
 import net.minestom.server.instance.IChunkLoader;
 import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.instance.block.Block;
+import net.minestom.server.instance.block.BlockHandler;
 import net.minestom.server.instance.generator.GenerationUnit;
 import net.minestom.server.instance.generator.Generator;
+import net.minestom.server.inventory.Inventory;
+import net.minestom.server.inventory.InventoryType;
+import net.minestom.server.utils.NamespaceID;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -28,6 +33,19 @@ public class OverWorld extends InstanceContainer {
         public void generate(@NotNull GenerationUnit unit) {
             // TODO: 27.05.22 generate overworld
             unit.modifier().fillHeight(-64, -63, Block.SAND);
+            unit.modifier().fillHeight(-63, -62, Block.CRAFTING_TABLE.withHandler(new BlockHandler() {
+
+                @Override
+                public boolean onInteract(@NotNull Interaction interaction) {
+                    interaction.getPlayer().openInventory(new Inventory(InventoryType.CRAFTING, Component.text("NYI")));
+                    return BlockHandler.super.onInteract(interaction);
+                }
+
+                @Override
+                public @NotNull NamespaceID getNamespaceId() {
+                    return NamespaceID.from("strawberry:handler_crafting_table");
+                }
+            }));
             unit.modifier().fillBiome(Biomes.DESERT);
         }
     }
